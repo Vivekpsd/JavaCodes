ef02ae4a36f37fd83ecbb05ae86ea3b8e0f74b1e
Vivek Prasad <Vivekpsd01@gmail.com>
2023-06-04 14:47:38 +0530

    MenuBar

---
+++ GUI/12_MenuBar/Demo.java
@@ -0,0 +1,7 @@
+import MenuPack.MyFrame;
+
+public class Demo {
+    public static void main(String[] args) {
+        new MyFrame();
+    }
+}
---
+++ GUI/12_MenuBar/MenuPack/MyFrame.java
@@ -0,0 +1,49 @@
+package MenuPack;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyFrame extends JFrame implements ActionListener  {
+
+    JMenuItem loadItem;
+
+    public MyFrame() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500, 500);
+        this.setLayout(new FlowLayout());
+
+        JMenuBar menubar = new JMenuBar();
+        JMenu fileMenu = new JMenu("File");
+        JMenu editMenu = new JMenu("Edit");
+        JMenu helpMenu = new JMenu("Help");
+
+        loadItem = new JMenuItem("Load");
+        JMenuItem saveItem = new JMenuItem("Save");
+        JMenuItem exitItem = new JMenuItem("Exit");
+
+        fileMenu.add(loadItem);
+        fileMenu.add(saveItem);
+        fileMenu.add(exitItem);
+
+        loadItem.addActionListener(this);
+
+        menubar.add(fileMenu);
+        menubar.add(editMenu);
+        menubar.add(helpMenu);
+
+
+        this.setJMenuBar(menubar);
+
+        this.setVisible(true);
+    }
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == loadItem) {
+            System.out.println("File Loading...");
+        }
+    }
+}
